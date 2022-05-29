ba9f93962a784c2d7dcb6cfb3633e03feae80701
wh1te909 <dcparsi@gmail.com>
2022-05-29 18:39:26 +0000

    update deps and browserlist

--- quasar.config.js
+++ quasar.config.js
@@ -51,7 +51,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
       target: {
-        browser: ["es2019", "edge88", "firefox78", "chrome87", "safari13.1"],
+        browser: ["es2021"],
         node: "node16",
       },
 
