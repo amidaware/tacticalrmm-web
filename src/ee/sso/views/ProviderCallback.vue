64daf290472f3335d4e801d209f49060d54c33b9
sadnub <josh@torchlake.com>
2024-09-15 22:32:37 -0400

    move sso auth to auth store

--- src/ee/sso/views/ProviderCallback.vue
+++ src/ee/sso/views/ProviderCallback.vue
@@ -19,10 +19,17 @@ const router = useRouter();
 const auth = useAuthStore();
 
 if (!error) {
-  if (auth.loggedIn) {
-    router.push({ name: "Dashboard" });
-  } else {
-    router.push({ name: "Login" });
-  }
+  auth
+    .checkSessionAuth()
+    .then((r) => {
+      // auth was successful
+      console.log(r);
+      router.push({ name: "Dashboard" });
+    })
+    .catch((e) => {
+      // auth was not successful
+      console.error(e);
+      router.push({ name: "Login" });
+    });
 }
 </script>
