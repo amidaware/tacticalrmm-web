4add8c25c6fb22070e15845cd442e294fd14951d
wh1te909 <7434746+wh1te909@users.noreply.github.com>
2025-09-17 18:36:31 +0000

    fix values

--- src/core/settings/components/ScheduleForm.vue
+++ src/core/settings/components/ScheduleForm.vue
@@ -299,13 +299,13 @@ const taskTypeOptions = [
 ];
 
 const dayOfWeekOptions = [
-  { label: "Monday", value: 1 },
-  { label: "Tuesday", value: 2 },
-  { label: "Wednesday", value: 3 },
-  { label: "Thursday", value: 4 },
-  { label: "Friday", value: 5 },
-  { label: "Saturday", value: 6 },
-  { label: "Sunday", value: 0 },
+  { label: "Monday", value: 0 },
+  { label: "Tuesday", value: 1 },
+  { label: "Wednesday", value: 2 },
+  { label: "Thursday", value: 3 },
+  { label: "Friday", value: 4 },
+  { label: "Saturday", value: 5 },
+  { label: "Sunday", value: 6 },
 ];
 
 const dayOfMonthOptions = (() => {
